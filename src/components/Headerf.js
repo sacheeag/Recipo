1846d124951c6494f66a13f5fdfbc91b7bc3609b
sacheeag <sachee.agarwal@gmail.com>
2025-04-13 21:57:19 +0530

    mobile responsive

--- src/components/Headerf.js
+++ src/components/Headerf.js
@@ -1,25 +1,40 @@
-import Logo from '../images/Logom.png'
-import { useNavigate } from 'react-router-dom'
+import React, { useState } from 'react';
+import Logo from '../images/Logom.png';
+import { useNavigate } from 'react-router-dom';
 
 function Headerf() {
     const navigate = useNavigate();
+    const [menuActive, setMenuActive] = useState(false);
+    
     const home = () => {
-        navigate('/')
-    }
-    const action2= ()=> {
-        navigate('/action2')
-    }
-    const login= () => {
-        navigate('/action3')
-    }
+        navigate('/');
+    };
+    
+    const action2 = () => {
+        navigate('/action2');
+    };
+    
+    const login = () => {
+        navigate('/action3');
+    };
+    
+    const toggleMenu = () => {
+        setMenuActive(!menuActive);
+    };
+    
     return (
         <>
         <div className="header">
+            <div className="mobile-menu-toggle" onClick={toggleMenu}>
+                <span></span>
+                <span></span>
+                <span></span>
+            </div>
             <div className="title">
                 <img src={Logo} alt="Sachee Agarwal 23BDS0087" />
                 <span onClick={home}>Recipo</span>
             </div>
-            <div className="nav-links">
+            <div className={`nav-links ${menuActive ? 'active' : ''}`}>
                 <a href="/">Home</a>
                 <a href="/">About</a>
                 <a href="/recipe">Recipes</a>
@@ -31,6 +46,7 @@ function Headerf() {
             </div>
         </div>
         </>
-    )
+    );
 }
+
 export default Headerf;
--- src/components/Headerf2.js
+++ src/components/Headerf2.js
@@ -1,21 +1,34 @@
-import Logo from '../images/Logom.png'
-import { useNavigate } from 'react-router-dom'
+import React, { useState } from 'react';
+import Logo from '../images/Logom.png';
+import { useNavigate } from 'react-router-dom';
 
 function Headerf2() {
     const navigate = useNavigate();
-    const home = () => {
-        navigate('/')
-    }
+    const [menuActive, setMenuActive] = useState(false);
     
+    const home = () => {
+        navigate('/');
+    };
+    
+    
+    
+    const toggleMenu = () => {
+        setMenuActive(!menuActive);
+    };
     
     return (
         <>
         <div className="header">
+            <div className="mobile-menu-toggle" onClick={toggleMenu}>
+                <span></span>
+                <span></span>
+                <span></span>
+            </div>
             <div className="title">
                 <img src={Logo} alt="Sachee Agarwal 23BDS0087" />
                 <span onClick={home}>Recipo</span>
             </div>
-            <div className="nav-links">
+            <div className={`nav-links ${menuActive ? 'active' : ''}`}>
                 <a href="/">Home</a>
                 <a href="/">About</a>
                 <a href="/recipe">Recipes</a>
@@ -24,6 +37,7 @@ function Headerf2() {
             
         </div>
         </>
-    )
+    );
 }
+
 export default Headerf2;
